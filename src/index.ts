cd6908d791c53bf477fdc2c21db86a1993e242c8
ccoellen <cristina.coellen@sciencespo.fr>
2022-11-19 18:21:54 +0100

    Ajout des nouvelles pages

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-import markdown from '../content.md?raw';
-import { marked } from "marked";
-import "./style.css";
+import homeSource from '../home.md?raw';
+import methodologySource from '../methodology.md?raw';
+import interviewSource from '../interviews.md?raw';
+import synthesisSource from '../synthesis.md?raw';
+import criticismSource from '../criticisms.md?raw';
+import { marked } from 'marked';
+import './style.css';
+import './header.css';
 
-document.getElementById('content').innerHTML =
-  marked.parse(markdown);
+
+const path = window.location.pathname;
+
+if (path === '/acled/') {
+  document.getElementById('content').innerHTML = marked.parse(homeSource);
+}
+
+if (path === '/acled/interviews') {
+  document.getElementById('content').innerHTML = marked.parse(interviewSource);
+}
+
+if (path === '/acled/methodology') {
+  document.getElementById('content').innerHTML = marked.parse(methodologySource);
+}
+
+if (path === '/acled/synthesis') {
+  document.getElementById('content').innerHTML = marked.parse(synthesisSource);
+}
+
+if (path === '/acled/criticisms') {
+  document.getElementById('content').innerHTML = marked.parse(criticismSource);
+}
